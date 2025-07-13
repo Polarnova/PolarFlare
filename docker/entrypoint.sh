df2093bc9240a3168fb7a589de2152ea5c03e6c1
Asher Yan <34890861+Polarnova@users.noreply.github.com>
2025-07-13 15:55:05 -0700

    Update Docker deployment for PolarFlare

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env ash
 
 # Start php-fpm and nginx
-chown -R nginx: /var/www/html/data/
-touch /var/www/html/data/index.php
+chown -R nginx: /var/www/html/data/polarflare/
+touch /var/www/html/data/polarflare/index.php
 php-fpm83
 nginx -c /etc/nginx/nginx.conf
 
 # Ready to serve?
 for i in 1 2 3; do
-  echo "Checking to see if FlashPaper is ready. ($i of 3)"
-  curl -sm3 localhost | grep -q "AndrewPaglusch/FlashPaper"
+  echo "Checking to see if PolarFlare is ready. ($i of 3)"
+  curl -sm3 localhost | grep -q "Polarnova/PolarFlare"
   if [[ $? -eq 0 ]]; then
-    echo "FlashPaper is ready."
+    echo "PolarFlare is ready."
     break
   fi
   sleep 2
-  echo "FlashPaper is not ready."
+  echo "PolarFlare is not ready."
 done
 
 echo "Access logging is disabled for production use. Tailing error logs..."
